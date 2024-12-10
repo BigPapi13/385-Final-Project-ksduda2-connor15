bf4f277f68de6ced4a76a85bfc76e4e252113cce
Connor Michalec <connormichalec@gmail.com>
2024-12-10 12:49:27 -0600

    Simultaion of 64x64 image at 16bpp waveform appears to be working mostly (some artifacting weird)

---
+++ HUB75_testing/HUB75_testing.srcs/sources_1/new/memory_initialization.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/10/2024 12:48:52 PM
+// Design Name: 
+// Module Name: memory_initialization
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module memory_initialization(
+
+    );
+endmodule
