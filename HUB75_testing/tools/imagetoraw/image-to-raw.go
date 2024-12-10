c71fd719ea4979ca757829ce9514bdde3dbeee18
Connor Michalec <connormichalec@gmail.com>
2024-12-10 12:49:27 -0600

    Simultaion of 64x64 image at 16bpp waveform appears to be working mostly (some artifacting weird)

---
+++ HUB75_testing/tools/imagetoraw/image-to-raw.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/sergeymakinen/go-bmp"
+)
+
+func output_bmp_as_raw(filename string) {
+	f, _ := os.Open(filename)
+	img, _ := bmp.Decode(f)
+
+	bounds := img.Bounds()
+
+	for y := 0; y < bounds.Dy(); y++ {
+		for x := 0; x < bounds.Dx(); x++ {
+			pixel := img.At(x, y)
+			r, g, b, _ := pixel.RGBA()
+			fmt.Printf("%02x%02x%02x00\n", r/256, g/256, b/256)
+		}
+	}
+}
+
+func main() {
+	inputFilenamePtr := flag.String("input-filename", "", "A bmp file")
+
+	flag.Parse()
+
+	if *inputFilenamePtr == "" {
+		fmt.Fprintf(os.Stderr, "--input-filename not specified\n")
+		os.Exit(1)
+	}
+
+	output_bmp_as_raw(*inputFilenamePtr)
+}
